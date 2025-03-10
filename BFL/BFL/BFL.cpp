0e11158cd0efe8a88e2a9bb62badd18d27eb0ad5
unknown <tsfkafsu@yandex.ru>
2025-03-10 23:00:52 +0500

    file create

--- BFL/BFL/BFL.cpp
+++ BFL/BFL/BFL.cpp
@@ -6,5 +6,5 @@ using namespace BFL;
 
 int main()
 {
-	everythingInDirectoryRecursive("F:/ssss",false);
+	
 }
--- BFL/BFL/BFL.hpp
+++ BFL/BFL/BFL.hpp
@@ -2,11 +2,17 @@
 
 #include <filesystem>
 #include <vector>
+#include <fstream>
 
 namespace BFL
 {
 	using namespace std;
 	using namespace std::filesystem;
+
+	bool isFile(path filePath)
+	{
+		return filePath.string().back() != '/';
+	}
 
 	string fileName(path filePath)
 	{
@@ -18,91 +24,68 @@ namespace BFL
 		return path1.string() + path2.string();
 	}
 
-	std::vector<std::filesystem::path> everythingInDirectory(path directory)
+	std::vector<std::filesystem::path> everythingInDirectory(path directory, bool throwError = true)
 	{
 		vector<path> files;
 		if (exists(directory))
 		{
-			for (const auto entry : directory_iterator(directory))
-				files.push_back(entry);
-		}
-		else
-		{
-			throw "directory not exist";
-		}
-
-		return files;
-	}
-	std::vector<std::filesystem::path> everythingInDirectory(path directory, bool throwError)
-	{
-		vector<path> files;
-		if (exists(directory))
-		{
-			for (const auto entry : directory_iterator(directory))
-				files.push_back(entry);
+			if (!isFile(directory))
+			{
+				for (const auto entry : directory_iterator(directory))
+					files.push_back(entry);
+			}
+			else
+			{
+				if (throwError)
+					throw "not a directory";
+			}
 		}
 		else
 		{
 			if (throwError)
-			{
 				throw "directory not exist";
+		}
+		return files;
+	}
+
+	std::vector<std::filesystem::path> everythingInDirectoryRecursive(path directory,bool throwError = true)
+	{
+		vector<path> files;
+		if (exists(directory))
+		{
+			if (!isFile(directory))
+			{
+				for (const auto entry : recursive_directory_iterator(directory))
+					files.push_back(entry);
+			}
+			else
+			{
+				if (throwError)
+					throw "not a directory";
 			}
 		}
-		return files;
-	}
-
-	std::vector<std::filesystem::path> everythingInDirectoryRecursive(path directory)
-	{
-		vector<path> files;
-		if (exists(directory))
-		{
-			for (const auto entry : recursive_directory_iterator(directory))
-				files.push_back(entry);
-		}
 		else
 		{
-			throw "directory not exist";
-		}
-		return files;
-	}
-	std::vector<std::filesystem::path> everythingInDirectoryRecursive(path directory,bool throwError)
-	{
-		vector<path> files;
-		if (exists(directory))
-		{
-			for (const auto entry : recursive_directory_iterator(directory))
-				files.push_back(entry);
-		}
-		else
-		{
-			if(throwError)
+			if (throwError)
 				throw "directory not exist";
 		}
 		return files;
 	}
 
-	unsigned long long directorySize(path directory)
+	unsigned long long itemSize(path directory, bool throwError = true)
 	{
 		unsigned long long result = 0;
 		if (exists(directory))
 		{
-			for (const auto i : everythingInDirectoryRecursive(directory))
-				result += file_size(i);
-		}
-		else
-		{
-			throw "directory not exist";
-		}
-
-		return(result);
-	}
-	unsigned long long directorySize(path directory, bool throwError)
-	{
-		unsigned long long result = 0;
-		if (exists(directory))
-		{
-			for (const auto i : everythingInDirectoryRecursive(directory))
-				result += file_size(i);
+			if (!isFile(directory))
+			{
+				for (const auto i : everythingInDirectoryRecursive(directory))
+					result += file_size(i);
+			}
+			else
+			{
+				result = file_size(directory);
+			}
 		}
 		else
 		{
@@ -111,5 +94,19 @@ namespace BFL
 		}
 
 		return(result);
+	}
+
+	void create(path dirPath,path name)
+	{
+		if (isFile(connectPaths(dirPath, name)))
+		{
+			ofstream out;
+			out.open(connectPaths(dirPath, name));
+			out.close();
+		}
+		else
+		{
+			create_directory(connectPaths(dirPath, name));
+		}
 	}
 }
